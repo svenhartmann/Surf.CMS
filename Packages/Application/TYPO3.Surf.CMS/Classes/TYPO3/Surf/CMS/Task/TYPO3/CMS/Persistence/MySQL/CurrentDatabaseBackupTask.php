fa589c9ccfd53f82f5bb77ac27424865a9e88a16
svenhartmann <hartmann@zweizunull.de>
2015-01-27 18:05:16 +0100

    [BUGFIX] Use options instead of env vars

--- Packages/Application/TYPO3.Surf.CMS/Classes/TYPO3/Surf/CMS/Task/TYPO3/CMS/Persistence/MySQL/CurrentDatabaseBackupTask.php
+++ Packages/Application/TYPO3.Surf.CMS/Classes/TYPO3/Surf/CMS/Task/TYPO3/CMS/Persistence/MySQL/CurrentDatabaseBackupTask.php
@@ -32,8 +32,8 @@ class CurrentDatabaseBackupTask extends Task {
      */
     public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
         $currentPath = $application->getDeploymentPath() . '/releases/current';
-        $mysqlDump = 'mysqldump -u' . getenv('DB_TARGET_USER') . ' -p' . getenv('DB_TARGET_PASS') . ' -h' . getenv('DB_TARGET_HOST');
-        $mysqlDump .= ' --default-character-set=utf8 --opt --skip-lock-tables --skip-add-locks --lock-tables=false ' . getenv('DB_TARGET_DBNAME') . ' > '.$currentPath.'/backup.sql';
+        $mysqlDump = 'mysqldump -u' . $options['targetUser'] . ' -p' . $options['targetPassword'] . ' -h' . $options['targetHost'];
+        $mysqlDump .= ' --default-character-set=utf8 --opt --skip-lock-tables --skip-add-locks --lock-tables=false ' . $options['targetDatabase'] . ' > '.$currentPath.'/backup.sql';
 
         $commands[] = $mysqlDump;
 
